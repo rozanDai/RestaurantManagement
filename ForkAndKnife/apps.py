15c456bbda3640a528ac48eccbd77c8cbf725212
sushanmhrzn277 <sushanmhrzn277@gmail.com>
2023-04-25 22:29:54 +0545

    Bootstrapped

--- ForkAndKnife/apps.py
+++ ForkAndKnife/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ForkandknifeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "ForkAndKnife"
+
+def myFunction(name):
+    print(name);
